fbfb36a8cc03bbe8f5562d4cdc53bf54e258b205
aagnelly <aagnelly@yahoo.com>
2016-10-12 18:37:18 -0400

    simulation script

--- gas_simulation2.js
+++ gas_simulation2.js
@@ -1,14 +1,3 @@
-/**
- * Heavily inspired by Hakim's (@hakimel( 
- * experiments with canvas!
- */
-
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
- 
-// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
- 
-// MIT license
 //textbox inputs/outputs
 
 
